a9be71ee39dbabd6cca0b0fb69750f48e71bfe27
Muhammadbilalhaider <haiderb894@gmail.com>
2025-01-25 02:48:11 +0500

    making Applied

--- src/components/jobFeed/JobFeed.tsx
+++ src/components/jobFeed/JobFeed.tsx
@@ -57,10 +57,10 @@ const JobFeed = () => {
         />
       </div>
 
-      <div className="flex flex-col p-5 items-start bg-white w-full md:w-10/12 lg:w-8/12 shadow-lg rounded-lg">
+      <div className="flex flex-col p-5 items-start bg-white w-full md:w-11/12 lg:w-8/12 shadow-lg rounded-lg">
         {/* Job Title */}
-        <div className="mb-4">
-          <h5 className="font-bold mb-2 text-neutral-dark">Journeyman Plumber</h5>
+        <div className="lg:mb-4 w-full">
+          <p className="font-bold lg:text-lg text-base lg:text-start md:text-start text-center mb-2 text-neutral-dark">Journeyman Plumber</p>
         </div>
 
         {/* Job Details */}
@@ -69,23 +69,23 @@ const JobFeed = () => {
             <span className="text-blue-500">
               <Building2 size={18} />
             </span>
-            <p className="text-base text-gray-600 ml-2">G&C Plumbing & Heating LLC</p>
+            <p className="lg:text-base md:text-sm text-xs text-gray-600 ml-2">G&C Plumbing & Heating LLC</p>
           </div>
           <div className="flex items-center">
             <span className="text-green-700">
               <MapPin size={18} />
             </span>
-            <p className="text-base text-gray-600 ml-2">Bellingham, MA, US</p>
+            <p className="lg:text-base md:text-sm text-xs text-gray-600 ml-2">Bellingham, MA, US</p>
           </div>
           <div className="flex items-center">
             <span className="text-orange-500">
               <Banknote size={18} />
             </span>
-            <p className="text-base text-gray-600 ml-2">$72,800 - $115,000</p>
+            <p className="lg:text-base md:text-sm text-xs text-gray-600 ml-2">$72,800 - $115,000</p>
           </div>
           <div className="flex items-center ">
             <span className='flex text-primary'><Earth size={18} /></span>
-            <p className="text-base ml-2 text-gray-600">Remote, Hybrid, On-Location</p>
+            <p className="lg:text-base md:text-sm text-xs ml-2 text-gray-600">Remote, Hybrid, On-Location</p>
           </div>
         </div>
 
@@ -95,10 +95,12 @@ const JobFeed = () => {
         </div>
 
         {/* About Section */}
-        <p className="text-lg font-semibold text-neutral-dark mb-2">About G&C Plumbing & Heating LLC</p>
-        <p className="text-gray-600 leading-relaxed">
+     <div className='w-full'>
+     <p className="lg:text-lg md:text-base text-sm lg:text-start md:text-start text-center font-semibold text-neutral-dark mb-2">About G&C Plumbing & Heating LLC</p>
+        <p className="text-gray-600 lg:text-base md:text-sm text-xs lg:text-start md:text-start text-center leading-relaxed">
           G&C Plumbing & Heating is a company dedicated to providing exceptional plumbing services while fostering a supportive and growth-oriented work environment.
         </p>
+     </div>
 
         {/* Separator */}
         <div className="w-full py-6">
@@ -106,31 +108,33 @@ const JobFeed = () => {
         </div>
 
         {/* Position Overview */}
-        <p className="text-lg font-semibold text-neutral-dark mb-2">Position Overview</p>
-        <p className="leading-relaxed text-gray-600">
+        <div className='w-full'>
+        <p className="lg:text-lg text-base lg:text-start md:text-start text-center font-semibold text-neutral-dark mb-2">Position Overview</p>
+        <p className="leading-relaxed lg:text-base md:text-sm text-xs lg:text-start md:text-start text-center text-gray-600">
           The Journeyman Plumber will provide top-notch plumbing services to residential customers, focusing on service calls and maintenance. The role offers growth opportunities, competitive pay, and a supportive team culture.
         </p>
+        </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-4 w-full">
           <div className="flex flex-col w-full">
             <span className="items-center flex flex-row ">
              <span className='text-blue-500 '><Briefcase size={18} /></span> 
-              <p className="text-base ml-2">Experience Required</p>
+              <p className="lg:text-base md:text-sm text-xs ml-2">Experience Required</p>
             </span>
 
 
-            <p className="text-base ml-7">NaN</p>
+            <p className="lg:text-base md:text-sm text-xs ml-7">NaN</p>
 
           </div>
 
           <div className="flex flex-col">
             <span className="items-center  flex -row ">
             <span className='text-[#038134] '><Clock size={18} /> </span>
-              <p className="text-base ml-2">Work Schedule</p>
+              <p className="lg:text-base md:text-sm text-xs ml-2">Work Schedule</p>
             </span>
 
 
-            <p className="text-base ml-7">Flexible 8-hour shifts, day shift, rotating weekends, year-round work</p>
+            <p className="lg:text-base md:text-sm text-xs ml-7">Flexible 8-hour shifts, day shift, rotating weekends, year-round work</p>
 
           </div>
         </div>
@@ -138,11 +142,11 @@ const JobFeed = () => {
 
 
         {/* Key Responsibilities */}
-        <div className="mt-6">
-          <p className="text-lg text-neutral-dark font-semibold mb-2">Key Responsibilities</p>
+        <div className="w-full mt-6">
+          <p className="lg:text-lg text-base lg:text-start text-center md:text-start text-neutral-dark font-semibold mb-2">Key Responsibilities</p>
           <ul className="list-disc ml-5 space-y-3">
             {keyResponsibilities.map((responsibility, index) => (
-              <li key={index} className="text-gray-600 leading-relaxed">
+              <li key={index} className="lg:text-base md:text-sm text-xs text-gray-600 leading-relaxed">
                 {responsibility}
               </li>
             ))}
@@ -153,11 +157,11 @@ const JobFeed = () => {
 
 
         {/* Requirements */}
-        <div className="mt-6">
-          <p className="text-lg text-neutral-dark font-semibold mb-2">Requirements</p>
+        <div className="w-full mt-6">
+          <p className="lg:text-lg text-base text-neutral-dark lg:text-start md:text-start text-center font-semibold mb-2">Requirements</p>
           <ul className="list-disc ml-5 space-y-3">
             {keyRequirements.map((requirement, index) => (
-              <li key={index} className="text-gray-600 leading-relaxed">
+              <li key={index} className="lg:text-base md:text-sm text-xs text-gray-600 leading-relaxed">
                 {requirement}
               </li>
             ))}
@@ -165,32 +169,33 @@ const JobFeed = () => {
         </div>
 
         {/* Soft Skills */}
-        <div className="mt-6">
-          <p className="text-lg text-neutral-dark font-semibold mb-2">Soft Skills</p>
-          <div className="flex p-0 gap-2">
+        <div className="mt-6 flex flex-col lg:w-7/12 md:w-9/12 w-full">
+          <p className="lg:text-lg text-base lg:text-start md:text-start text-center text-neutral-dark font-semibold mb-2">Soft Skills</p>
+          <div className="w-full grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 p-0 gap-2 sm:items-center sm:justify-center">
             <div className="bg-accent-light justify-center items-center px-3 rounded-full">
-              <p className="text-accent-secondary p-1 text-sm font-medium text-center">Customer-focused attitude</p>
+              <p className="text-accent-secondary p-0.5 text-sm sm:text-xs font-medium text-center">Customer-focused attitude</p>
             </div>
             <div className="bg-accent-light justify-center items-center px-3 rounded-full">
-            <p className="text-accent-secondary p-1 text-sm font-medium text-center">Commitment to quality work</p>
+            <p className="w-full text-accent-secondary p-0.5 text-sm sm:text-xs font-medium text-center">Commitment to quality work</p>
             </div>
           </div>
         </div>
 
         {/* Required Technologies */}
 
-        <div className="mt-6">
-          <p className="text-lg text-neutral-dark font-semibold mb-2">Required Technologies</p>
-          <div className="flex p-0 gap-2">
+        <div className="mt-6 flex flex-col lg:w-7/12 md:w-9/12 w-full">
+          <p className="lg:text-lg text-base lg:text-start md:text-start text-center text-neutral-dark font-semibold mb-2">Required Technologies</p>
+          <div className="w-full grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 p-0 gap-2 sm:items-center sm:justify-center">
             <div className="bg-accent-light justify-center items-center px-3 rounded-full">
-              <p className="text-accent-secondary p-1 text-sm font-medium text-center">Customer-focused attitude</p>
+              <p className="text-accent-secondary p-0.5 text-sm sm:text-xs font-medium text-center">Customer-focused attitude</p>
             </div>
             <div className="bg-accent-light justify-center items-center px-3 rounded-full">
-            <p className="text-accent-secondary p-1 text-sm font-medium text-center">Commitment to quality work</p>
+            <p className="w-full text-accent-secondary p-0.5 text-sm sm:text-xs font-medium text-center">Commitment to quality work</p>
             </div>
           </div>
-         
         </div>
+
+
          {/* Separator */}
          <div className="w-full py-3 mt-6">
           <hr className="border border-gray-400 opacity-40" />
